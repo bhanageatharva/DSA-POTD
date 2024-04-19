6ae1c12fa096fe56527a625934f506888d6bc3c1
bhanageatharva <87986946+bhanageatharva@users.noreply.github.com>
2024-04-19 23:17:24 +0530

    Create Find missing in second array.java

---
+++ Find missing in second array -GFG/Find missing in second array.java
@@ -0,0 +1,17 @@
+class Solution
+{
+   
+    ArrayList<Integer> findMissing(int a[], int b[], int n, int m)
+    {
+         ArrayList<Integer> al = new ArrayList<>();
+         Set<Integer> set = new HashSet<>();
+         for(int x : b){
+             set.add(x);
+         }
+         for(int x: a){
+             if(!set.contains(x))
+             al.add(x);
+         }
+         return al;
+    }
+}
